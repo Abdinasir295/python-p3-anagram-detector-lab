1f43151f49a8dae746dac65dcc0ed88329c45920
Abdinasir Dahir <abdinasir.dahir@moringaschool.com>
2024-09-04 19:37:31 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+
+    def match(self, word_list):
+        matching_words = []
+        for word in word_list:
+            if ''.join(sorted(word)) == self.sorted_word:
+                matching_words.append(word)
+        return matching_words
